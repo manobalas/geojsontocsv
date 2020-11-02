b9be4543b87a18bc781003e6caf896f83f1dd7d1
Manobala S <manobala.s@Hotmail.com>
2020-11-02 23:30:45 +0530

    Update index.js

--- pim360/index.js
+++ pim360/index.js
@@ -3,7 +3,10 @@ const convertfun = require('./functions/convert');
 module.exports = async function (context, req) {
     let response = await convertfun.do(req)
     context.res = {
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'text/csv',
+            "Content-Disposition": `attachment; filename=${"geojsontocsv-" + new Date().getTime() + ".csv"}`
+        },
         body: response
     };
 };
