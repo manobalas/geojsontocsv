2b36377057ed8d82b7e72389908d0c176cd9780a
Manobala S <manobala.s@Hotmail.com>
2020-11-02 23:23:02 +0530

    up

--- pim360/index.js
+++ pim360/index.js
@@ -1,48 +1,9 @@
-var multipart = require("parse-multipart");
-const fs = require('fs-extra');
-const json2csv = require("json2csv").parse;
-
+const convertfun = require('./functions/convert');
 
 module.exports = function (context, req) {
-
-    const convert = (request) => {
-        return new Promise((resolve, reject) => {
-            // encode body to base64 string
-            var bodyBuffer = Buffer.from(request.body);
-            // get boundary for multipart data e.g. ------WebKitFormBoundaryDtbT5UpPj83kllfw
-            var boundary = multipart.getBoundary(request.headers['content-type']);
-            // parse the body
-            var parts = multipart.Parse(bodyBuffer, boundary);
-            // var file = parts[0].data;
-
-            let json = JSON.stringify(parts[0].data);
-            let bufferOriginal = Buffer.from(JSON.parse(json).data);
-            let finalJSONData = JSON.parse(bufferOriginal.toString());
-            // fs.writeFileSync('D:/local/Temp/settings.json', JSON.stringify(parts[0].data))
-
-            let arrObj = [];
-            finalJSONData.features.map((i) => {
-                arrObj.push({
-                    "Properties.Station": i.properties.Station,
-                    "Geometry.Latitude": i.geometry.coordinates[0],
-                    "Geometry.Longitude": i.geometry.coordinates[1]
-                })
-            })
-
-            if (arrObj.length > 0) {
-                let fields = Object.keys(arrObj[0]);
-                const csv = json2csv(arrObj, fields);
-                resolve(csv)
-            } else {
-                resolve({ "message": "No data / Something went wrong" })
-            }
-        })
-    }
-
-    let response = await convert(req);
-
+    let response = await convertfun.do(req)
     context.res = {
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: response
     };
 };
