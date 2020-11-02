88e0f290e8169c2f18919c4e33ae0d6a502f4f95
Manobala S <manobala.s@Hotmail.com>
2020-11-02 19:37:10 +0530

    another

--- pim360/index.js
+++ pim360/index.js
@@ -1,49 +1,63 @@
-const createHandler = require("azure-function-express").createHandler;
-const express = require("express");
-const multer = require('multer');
+// const createHandler = require("azure-function-express").createHandler;
+// const express = require("express");
+// const multer = require('multer');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'D:/local/Temp/');
-    },
-    filename: (req, file, cb) => {
-        console.log(file);
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
-});
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype == 'application/json') {
-        cb(null, true);
-    } else {
-        cb(null, false);
-    }
-}
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'D:/local/Temp/');
+//     },
+//     filename: (req, file, cb) => {
+//         console.log(file);
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     }
+// });
+// const fileFilter = (req, file, cb) => {
+//     if (file.mimetype == 'application/json') {
+//         cb(null, true);
+//     } else {
+//         cb(null, false);
+//     }
+// }
+// const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-// Create express app as usual
-const app = express();
+// // Create express app as usual
+// const app = express();
 
-// app.post("/api/upload", (req, res) => {
+// // app.post("/api/upload", (req, res) => {
 
 
-//     // reponse
-//     res.json({
-//         foo: req.params.foo,
-//         bar: req.params.bar
-//     });
+// //     // reponse
+// //     res.json({
+// //         foo: req.params.foo,
+// //         bar: req.params.bar
+// //     });
+// // });
+
+// app.post('/api/upload', upload.single('file'), (req, res, next) => {
+//     try {
+//         res.json({
+//             "message": "file got it"
+//         });
+//     } catch (error) {
+//         res.json({
+//             "message": "error"
+//         });
+//     }
 // });
 
-app.post('/api/upload', upload.single('file'), (req, res, next) => {
-    try {
-        res.json({
-            "message": "file got it"
-        });
-    } catch (error) {
-        res.json({
-            "message": "error"
-        });
-    }
-});
+// // Binds the express app to an Azure Function handler
+// module.exports = createHandler(app);
 
-// Binds the express app to an Azure Function handler
-module.exports = createHandler(app);
+var multipart = require("parse-multipart");
+
+module.exports = function (context, request) {  
+    context.log('JavaScript HTTP trigger function processed a request.'); 
+    // encode body to base64 string
+    var bodyBuffer = Buffer.from(request.body);
+    // get boundary for multipart data e.g. ------WebKitFormBoundaryDtbT5UpPj83kllfw
+    var boundary = multipart.getBoundary(request.headers['content-type']);
+    // parse the body
+    var parts = multipart.Parse(bodyBuffer, boundary);
+    context.res = { body : { name : parts[0].filename, type: parts[0].type, data: parts[0].data.length}}; 
+    context.done();  
+};
