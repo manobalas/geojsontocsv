b2d4c1fbd6714b8aa70a891b9fb9e407f166c213
Manobala S <manobala.s@Hotmail.com>
2020-11-10 13:30:43 +0530

    Update index.js

--- pim360/index.js
+++ pim360/index.js
@@ -1,22 +1,16 @@
 const convertfun = require('./functions/convert');
 const download = require('./functions/download');
-var multipart = require("parse-multipart");
 
 module.exports = async function (context, req) {
     let response = {};
     const function_name = (req.query.function_name || "geojsontocsv");
-    let file_name = (req.query.file_name || "");
+    let file_name = function_name == 'geojsontocsvflowversion' ? (req.body.body.filename || "") : (req.query.file_name || "");
 
     switch (function_name) {
         case "geojsontocsv":
             response = await convertfun.do(req)
             break;
-        case "geojsontocsvflowversion":            
-            var bodyBuffer = Buffer.from(request.body);
-            var boundary = multipart.getBoundary(request.headers['content-type']);
-            var parts = multipart.Parse(bodyBuffer, boundary);
-            let orgFileName = parts[0].filename;
-            file_name = (orgFileName || "");
+        case "geojsontocsvflowversion":
             response = await convertfun.dojson(req)            
             break;
         case "geojsontocsvflowversionextendeddownload":
