94c7ce6f351c3d81435e23c926bf99c5e23f3820
Manobala S <manobala.s@Hotmail.com>
2020-12-14 22:07:06 +0530

    Update convertLineStringToCSV.js

--- pim360/functions/convertLineStringToCSV.js
+++ pim360/functions/convertLineStringToCSV.js
@@ -110,19 +110,19 @@ const convert = function (request) {
                                     } else {
                                         // others
                                         let dist = distanceCalc(
-                                            parseFloat(i.geometry.coordinates[index0][index][1]),
-                                            parseFloat(i.geometry.coordinates[index0][index][0]),
-                                            parseFloat(i.geometry.coordinates[index0][index + 1][1]),
-                                            parseFloat(i.geometry.coordinates[index0][index + 1][0])
+                                            parseFloat(coordinates[index][1]),
+                                            parseFloat(coordinates[index][0]),
+                                            parseFloat(coordinates[index + 1][1]),
+                                            parseFloat(coordinates[index + 1][0])
                                         );
                                         totalM = totalM + dist;
                                         cumulative_totalM = cumulative_totalM + dist;
                                         arrObj.push({
                                             ...newObj,
-                                            "Geometry.Start.Latitude": i.geometry != null ? i.geometry.coordinates[index0][index][1] : "No Data",
-                                            "Geometry.Start.Longitude": i.geometry != null ? i.geometry.coordinates[index0][index][0] : "No Data",
-                                            "Geometry.End.Latitude": i.geometry != null ? i.geometry.coordinates[index0][index + 1][1] : "No Data",
-                                            "Geometry.End.Longitude": i.geometry != null ? i.geometry.coordinates[index0][index + 1][0] : "No Data",
+                                            "Geometry.Start.Latitude": i.geometry != null ? coordinates[index][1] : "No Data",
+                                            "Geometry.Start.Longitude": i.geometry != null ? coordinates[index][0] : "No Data",
+                                            "Geometry.End.Latitude": i.geometry != null ? coordinates[index + 1][1] : "No Data",
+                                            "Geometry.End.Longitude": i.geometry != null ? coordinates[index + 1][0] : "No Data",
                                             "Distance in Kilo Meters": totalM / 1000,
                                             "Cumulative Distance in Kilo Meters": cumulative_totalM / 1000,
                                             "Distance in Miles": getMiles(totalM),
